95df5392043a34312ea421bd187d79ac102fd23c
Mark Gruzdov <grumark198@gmail.com>
2024-01-14 21:38:31 +0300

    fix brain-progression game

--- src/utils/get-progression-question.js
+++ src/utils/get-progression-question.js
@@ -4,7 +4,7 @@ export default function getProgressionQuestion(context) {
   const progression = [];
   progression.length = getRandomInt(5, 10);
   const firstValue = getRandomInt(1, 101);
-  const hiddenValue = getRandomInt(0, progression.length);
+  const hiddenValue = getRandomInt(0, progression.length - 1);
   const progressionStep = getRandomInt(1, 10);
 
   for (let count = 0; count < progression.length; count += 1) {
